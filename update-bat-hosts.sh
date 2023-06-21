433e61d585225fe6c271ad145708ee1e958f070e
Tony Wu <tonywu3027@me.com>
2023-06-21 19:23:52 +0100

    use sudo to remove old file in update-bat-hosts

--- update-bat-hosts.sh
+++ update-bat-hosts.sh
@@ -4,7 +4,7 @@ source utils.sh
 
 echo -e "${C_BLUE}Updating bat-hosts...${C_RESET}"
 
-rm -f /etc/bat-hosts
+sudo rm -f /etc/bat-hosts
 sudo cp ./bat-hosts /etc
 
 echo -e "${C_GREEN}Updating bat-hosts completes...${C_RESET}"
