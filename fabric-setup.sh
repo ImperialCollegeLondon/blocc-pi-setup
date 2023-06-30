74cf6bcc4176d9ab4cab1e32d43c644ef7d67ae6
Tony Wu <tonywu3027@me.com>
2023-06-30 15:35:44 +0100

    fabric-setup.sh: skip fabric installation if already installed

--- fabric-setup.sh
+++ fabric-setup.sh
@@ -17,17 +17,25 @@ sudo systemctl start docker
 echo -e "${C_BLUE}Adding Docker to this user group...${C_RESET}"
 sudo usermod -aG docker $USER
 
-echo -e "${C_BLUE}Creating fabric directroy...${C_RESET}"
-mkdir -p "$HOME"/fabric
-cd "$HOME"/fabric || exit
+FABRIC_DIR="$HOME/fabric"
 
-echo -e "${C_BLUE}Downloading Hyperledger Fabric installation script...${C_RESET}"
-curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
+if [ -d "$FABRIC_DIR" ]; then
+  echo -e "${C_YELLOW}Hyperledger Fabric is already installed. Skipping installation.${C_RESET}"
+else
+  echo -e "${C_BLUE}Creating fabric directory...${C_RESET}"
+  mkdir -p "$FABRIC_DIR"
+  cd "$FABRIC_DIR" || exit
 
-echo -e "${C_BLUE}Installing Hyperledger Fabric binaries...${C_RESET}"
-./install-fabric.sh binary
+  echo -e "${C_BLUE}Downloading Hyperledger Fabric installation script...${C_RESET}"
+  curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
 
-echo -e "${C_BLUE}Installing Hyperledger Fabric Docker images...${C_RESET}"
-sudo ./install-fabric.sh docker
+  echo -e "${C_BLUE}Installing Hyperledger Fabric binaries...${C_RESET}"
+  ./install-fabric.sh binary
+  echo -e "${C_GREEN}Hyperledger Fabric binaries installed.${C_RESET}"
+
+  echo -e "${C_BLUE}Installing Hyperledger Fabric Docker images...${C_RESET}"
+  sudo ./install-fabric.sh docker
+  echo -e "${C_GREEN}Hyperledger Fabric Docker images installed.${C_RESET}"
+fi
 
 echo -e "${C_YELLOW}Note that: you may need to log back in to use docker without sudo${C_RESET}"
