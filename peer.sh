18c3a61434625a6fc6ac06058f7918635eab95c9
Tony Wu <tonywu3027@me.com>
2023-06-30 15:24:46 +0100

    Prompt the progress in peer.sh

--- peer.sh
+++ peer.sh
@@ -5,15 +5,15 @@ set -e
 source utils.sh
 
 function peerUp() {
-    "${C_BLUE}Bringing up peer...${C_RESET}"
+    echo -e "${C_BLUE}Bringing up peer...${C_RESET}"
     docker-compose -f ~/fabric/compose-peer.yaml up -d
-    "${C_GREEN}Peer container is up${C_RESET}"
+    echo -e "${C_GREEN}Peer container is up${C_RESET}"
 }
 
 function peerDown() {
-    "${C_BLUE}Bringing down peer...${C_RESET}"
+    echo -e "${C_BLUE}Bringing down peer...${C_RESET}"
     docker-compose -f ~/fabric/compose-peer.yaml down --volumes --remove-orphans
-    "${C_GREEN}Peer container is removed${C_RESET}"
+    echo -e "${C_GREEN}Peer container is removed${C_RESET}"
 }
 
 
