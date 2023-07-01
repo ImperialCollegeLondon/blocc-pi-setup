1494e05471b67a279ab6c3102513519a2f1fa230
Tony Wu <tonywu3027@me.com>
2023-07-01 02:06:46 +0100

    fabric-setup: add peer compose file

--- fabric-setup.sh
+++ fabric-setup.sh
@@ -2,6 +2,59 @@
 
 source utils.sh
 
+function printHelp() {
+  echo -e "Usage: $0 ${C_BLUE}<mode> ${C_YELLOW}[options]${C_RESET}"
+  echo
+  echo -e "${C_BLUE}Modes:"
+  echo -e "  orderer        Set up orderer"
+  echo -e "  peer           Set up peer${C_RESET}"
+  echo
+  echo -e "${C_YELLOW}Options:"
+  echo -e "  -h, --help     Display this help message"
+  echo -e "  --org=<org_number>"
+  echo -e "                 Set the FABRIC_PEER_ORG_NUM environment variable for the peer"
+  echo -e "                 (Only applicable in peer mode)"
+  echo -e "                 Example: $0 peer --org=1${C_RESET}"
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  printHelp
+  exit 0
+fi
+
+if [ "$1" == "orderer" ]; then
+  echo -e "${C_BLUE}Adding orderer compose file to fabric directory...${C_RESET}"
+  cp ./compose/compose-orderer.yaml ~/fabric/
+elif [ "$1" == "peer" ]; then
+  ORG_FLAG="--org"
+
+  function setOrgEnvironmentVariable() {
+    local org_num="$1"
+    if ! grep -q "FABRIC_PEER_ORG_NUM" ~/.bashrc; then
+      echo "export FABRIC_PEER_ORG_NUM=$org_num" >> ~/.bashrc
+      source ~/.bashrc
+      echo "FABRIC_PEER_ORG_NUM has been set to: $org_num"
+    else
+      echo "FABRIC_PEER_ORG_NUM is already set in ~/.bashrc."
+    fi
+  }
+
+  if [[ "$2" == "$ORG_FLAG"* ]]; then
+    org_number="${2#*=}"
+    setOrgEnvironmentVariable "$org_number"
+  else
+    echo -e "${C_RED}Missing org number!${C_RESET}"
+    printHelp
+    exit 1
+  fi
+
+  echo -e "${C_BLUE}Adding peer compose file to fabric directory...${C_RESET}"
+  cp ./compose/compose-peer.yaml ~/fabric/
+else
+  printHelp
+  exit 1
+fi
+
 echo -e "${C_BLUE}Installing Docker...${C_RESET}"
 
 install_package docker-compose
@@ -43,11 +96,6 @@ if [[ ":$PATH:" != *":$FABRIC_DIR/bin:"* ]]; then
   echo -e "${C_BLUE}Adding Hyperledger Fabric binaries to PATH...${C_RESET}"
   echo 'export PATH="$HOME/fabric/bin:$PATH"' >> ~/.bashrc
   source ~/.bashrc
-fi
-
-if [ "$1" == "orderer" ] || [ "$1" == "peer" ]; then
-  echo -e "${C_BLUE}Adding ${1} compose file to fabric directory...${C_RESET}"
-  cp ./compose/compose-${1}.yaml ~/fabric/
 fi
 
 echo -e "${C_BLUE}Adding container control script to fabric directory...${C_RESET}"
